23ddf4f1c0d26a26fc5b91ce11bb974d9be76c30
MMajewski <majewskimichal92@gmail.com>
2024-01-19 20:37:59 +0100

    Ex1: Recursive interface check

---
+++ src/main/java/com/mmajewski/experimental/exercise/one/Exercise.java
@@ -0,0 +1,33 @@
+package com.mmajewski.experimental.exercise.one;
+
+import java.util.*;
+
+public class Exercise {
+
+    public static void main(String[] args) {
+        Set<Class<?>> allImplementedInterfaces = findAllImplementedInterfaces(ArrayList.class);
+        System.out.println(allImplementedInterfaces);
+    }
+
+    /**
+     * Returns all the interfaces that the current input class implements.
+     * Note: If the input is an interface itself, the method returns all the interfaces the
+     * input interface extends.
+     */
+    public static Set<Class<?>> findAllImplementedInterfaces(Class<?> input) {
+        Set<Class<?>> allImplementedInterfaces = new HashSet<>();
+
+        Class<?>[] inputInterfaces = input.getInterfaces();
+        for (Class<?> currentInterface : inputInterfaces) {
+            allImplementedInterfaces.add(currentInterface);
+
+            if (currentInterface.getInterfaces().length == 0) {
+                continue;
+            }
+
+            allImplementedInterfaces.addAll(findAllImplementedInterfaces(currentInterface));
+        }
+
+        return allImplementedInterfaces;
+    }
+}
